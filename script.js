96312743e28fdcbb1ab8ab171181b7139bba69c2
Eliška Šibrová <eliska.sibrova@tacr.cz>
2022-10-16 19:56:46 +0200

    Format documents and fix font size

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let menu = document.querySelector('.header__hamburger-menu');
-let nav = document.querySelector('.header__nav');
+let menu = document.querySelector(".header__hamburger-menu");
+let nav = document.querySelector(".header__nav");
 
-menu.addEventListener ('click', () => {
-    menu.classList.toggle('open');
-    nav.classList.toggle('open');
+menu.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  nav.classList.toggle("open");
 });
