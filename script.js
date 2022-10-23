f21e0fb68bf62ba77b5c29dedbc21d690943cde3
Eliška Šibrová <eliska.sibrova@tacr.cz>
2022-10-23 18:30:40 +0200

    Add slideshow

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// hamburger menu
+
 let menu = document.querySelector(".header__hamburger-menu");
 let nav = document.querySelector(".header__nav");
 
@@ -5,3 +7,30 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("open");
   nav.classList.toggle("open");
 });
+
+// slideshow
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("slides");
+
+  if (window.innerWidth <= 1050) {
+    if (n > x.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+  }
+}
