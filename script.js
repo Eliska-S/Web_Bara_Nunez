52b32d3131b85b1b867da8bf73ec655b4f72c1d7
Eliška Šibrová <eliska.sibrova@tacr.cz>
2022-10-28 17:35:55 +0200

    Remove slider

--- script.js
+++ script.js
@@ -7,30 +7,3 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("open");
   nav.classList.toggle("open");
 });
-
-// slideshow
-
-let slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs((slideIndex += n));
-}
-
-function showDivs(n) {
-  let i;
-  let x = document.getElementsByClassName("slides");
-
-  if (window.innerWidth <= 1050) {
-    if (n > x.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = x.length;
-    }
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    x[slideIndex - 1].style.display = "block";
-  }
-}
